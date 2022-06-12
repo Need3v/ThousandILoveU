58107057feb2ce4b33f85ff49151a0fdb5eaf2f0
Cauã Melo <86328823+Need3v@users.noreply.github.com>
2022-06-12 20:43:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+// FUNCTION TO EXECUTE SEND MESSAGE WITH "I LOVE U" MESSAGE
 async function executarMsg() {
-for (i = 1; i <= 10; i++) {
-    sendMessage("*"+i+"º* EU TE AMO ❤️");
-    console.log(i+"º EU TE AMO ❤️");
-    await new Promise(resolve => setTimeout(resolve, 250))
-}
+    for (i = 1; i <= 1000; i++) {
+        sendMessage("*"+i+"º* EU TE AMO ❤️");
+        console.log(i+"º EU TE AMO ❤️");
+        await new Promise(resolve => setTimeout(resolve, 250))
+    }
 }
 
+// FUNCTION TO SEND MESSAGE ON WHATSAPP
 function sendMessage (message) {
   window.InputEvent = window.Event || window.InputEvent;
 
@@ -23,4 +25,5 @@ function sendMessage (message) {
     
 }
 
+// EXECUTE
 executarMsg()
